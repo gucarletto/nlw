f7f04b5acb6c2a07bf168b574f95223b87c662b9
gucarletto <gucarletto@gmail.com>
2020-06-14 16:29:18 -0300

    Mobile pt1

--- api/src/controllers/ItemsController.ts
+++ api/src/controllers/ItemsController.ts
@@ -10,7 +10,7 @@ class ItemsController {
       return {
         id: item.id,
         titulo: item.titulo,
-        image_url: `http://localhost:4000/uploads/${item.image}`
+        image_url: `http://192.168.1.4:4000/uploads/${item.image}`
       }
     })
 
